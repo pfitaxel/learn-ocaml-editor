9e37788d35025a8f59d7f156ebf56c080edcc26e
Romain Grimal <romain.grimal@univ-tlse3.fr>
2018-05-23 17:31:44 +0200

    src/editor/new_exercise.ml

--- src/editor/new_exercise.ml
+++ src/editor/new_exercise.ml
@@ -1,33 +1,33 @@
 open Js_of_ocaml;;
 open Dom_html;;
+open Learnocaml_common;;
 
 let setInnerHtml elt s =    
-  elt##innerHTML<-Js.string s ;;
+  elt##innerHTML <- Js.string s;;
 
-(*
-let checkbox txt checked action =
-  let b = Dom_html.createInput ~_type:(Js.string "checkbox") doc in
-  b##.checked := Js.bool checked;
-  b##.onclick :=
-    Dom_html.handler (fun _ -> action (Js.to_bool b##.checked); Js._true);
-  let lab = Dom_html.createLabel doc in
-  Dom.appendChild lab b;
-  Dom.appendChild lab (doc##createTextNode (Js.string txt));
-  lab 
- *)
-
-
-
-
-let saveb=getElementById "save";;
-(*let title =getElementById "title" in
-let identity=getElementById "identity" in
-let description=getElementById "description" in*)
-let para =getElementById ("para") in
-let dificulty=getElementById_coerce "title" CoerceTo.input in
-match dificulty with
-| None ->
-    failwith "fail"
-|Some input -> 
- saveb##onclick<-handler (fun _ ->  setInnerHtml para (Js.to_string ( input##value) )  ;Js._true);; 
-
+let save = getElementById "save"
+and identifier = getElementById_coerce "identifier" CoerceTo.input in
+let id = match identifier with
+  | None -> failwith "incorrect_input"
+  | Some input -> Js.to_string input##value
+and title = getElementById_coerce "title" CoerceTo.input in
+let titre = match title with
+  | None -> failwith "incorrect_input"
+  | Some input -> Js.to_string input##value
+and descr = getElementById_coerce "description" CoerceTo.input in
+let description = match descr with
+  | None -> None
+  | Some input -> Some (Js.to_string input##value)
+and difficulty = getElementById_coerce "difficulty" CoerceTo.input in
+let diff = match difficulty with
+  | None -> None
+  | Some input -> float_of_string_opt (Js.to_string input##value)
+and report = None
+and solution = ""
+and question = ""
+and template = ""
+and test = "" in
+    save##onclick <- handler (fun _ ->
+      Learnocaml_local_storage.(store (editor_state id))
+        { Learnocaml_exercise_state.report ; id ; solution ; titre ; question ; template ; diff ; test ; description ;
+          mtime = gettimeofday () }; Js._true);;

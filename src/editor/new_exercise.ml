726e65c3b3f6c332b7852adce96add2997d01a11
Romain Grimal <romain.grimal@univ-tlse3.fr>
2018-05-24 10:24:07 +0200

    src/editor/new_exercise.ml

--- src/editor/new_exercise.ml
+++ src/editor/new_exercise.ml
@@ -1,3 +1,4 @@
+
 open Js_of_ocaml;;
 open Dom_html;;
 open Learnocaml_common;;
@@ -18,7 +19,7 @@ and descr = getElementById_coerce "description" CoerceTo.input in
 let description = match descr with
   | None -> None
   | Some input -> Some (Js.to_string input##value)
-and difficulty = getElementById_coerce "difficulty" CoerceTo.input in
+and difficulty = getElementById_coerce "difficulty" CoerceTo.select in
 let diff = match difficulty with
   | None -> None
   | Some input -> float_of_string_opt (Js.to_string input##value)
@@ -30,4 +31,6 @@ and test = "" in
     save##onclick <- handler (fun _ ->
       Learnocaml_local_storage.(store (editor_state id))
         { Learnocaml_exercise_state.report ; id ; solution ; titre ; question ; template ; diff ; test ; description ;
-          mtime = gettimeofday () }; Js._true);;
+          mtime = gettimeofday () };
+      Dom_html.window##location##assign (Js.string ("editor.html#id="^id^"&action=open"));
+      Js._true);;

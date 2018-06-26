e9189cfd2c0df9c618a542451e62ec999ac1b478
RuminS <sophie.rumin@univ-tlse3.fr>
2018-06-26 17:56:39 +0200

    save test

--- src/editor/testhaut.ml
+++ src/editor/testhaut.ml
@@ -34,17 +34,24 @@ let init_tabs, select_tab =
       names ;
     select_tab !current in
   init_tabs, select_tab 
-                                      
+
+let id = arg "id";;               
 let name = match getElementById_coerce "name" CoerceTo.input with
     None -> failwith "element inconnu"
    |Some s -> s;;
-let ty = getElementById_coerce "ty" CoerceTo.input;;
+let ty = match getElementById_coerce "ty" CoerceTo.input with
+    None -> failwith "element inconnu"
+   |Some s -> s;;
 let solution = getElementById "solution";;
 let spec = getElementById "spec";;
 let suite = getElementById "suite";;
-
-
-let para = getElementById ("txt_name");;
+let input_suite = match getElementById_coerce "input" CoerceTo.textarea with
+    None -> failwith "element inconnu"
+  | Some s -> s;;
+let output_suite = match getElementById_coerce "output" CoerceTo.textarea with
+    None -> failwith "element inconnu"
+  | Some s -> s;;
+let save = getElementById "save";;
 
 let setInnerHtml elt s =    
   elt##.innerHTML:=Js.string s ;;
@@ -52,3 +59,23 @@ let _ = solution##.onclick:= handler (fun _ -> select_tab "solution"; Js._true);
 
 let _ = spec##.onclick:= handler (fun _ -> select_tab "spec"; Js._true);;
 let _ = suite##.onclick:= handler (fun _ -> select_tab "suite"; Js._true);;
+let _ = save##.onclick:= handler (fun _ ->
+   if arg "tab" = "suite" then
+      begin
+        let open Learnocaml_exercise_state in
+        let name = Js.to_string name##.value in
+        let ty = Js.to_string ty##.value in
+        let type_question = Suite in
+        let input = Js.to_string input_suite##.value in
+        let output = Js.to_string  output_suite##.value in
+        let extra_alea = 0 in
+        let question = {name; ty; type_question; input; output; extra_alea} in
+        let open Editor_lib in
+        let testhaut = get_testhaut id in
+        let testhaut = StringMap.add "1" question testhaut in
+        save_testhaut testhaut id;
+        Dom_html.window##.location##assign
+        (Js.string ("editor.html#id=" ^ id ^ "&action=open"))
+        
+      end;
+   Js._true)

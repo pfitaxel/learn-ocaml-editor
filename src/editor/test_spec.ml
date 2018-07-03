c271ee7418319fd6ad5c3ada185f9e4889b1c8fd
RuminS <sophie.rumin@univ-tlse3.fr>
2018-07-03 12:15:57 +0200

    police et taille buffer

--- src/editor/test_spec.ml
+++ src/editor/test_spec.ml
@@ -99,11 +99,11 @@ let example_constr_suite =
 let local_dummy : 'a sampler = fun () -> failwith "dummy sampler"
 (* à n'utiliser que si on passe l'argument ~gen:0 (pas d'alea) *)
 
-let rec premierArg s arg = match s with
+(*let rec premierArg s arg = match s with
     [] -> arg
   | a::[] -> if a=' ' then [] else [a]
   | a::b::suite -> if a<>' ' then premierArg (b::suite) (arg@[a]) else arg;;
-let rec stringToSuite s = premierArg s :: stringToSuite (suite s)
+let rec stringToSuite s = premierArg s :: stringToSuite (suite s)*)
   
                                        
 let test_question (t : test_qst_typed) =

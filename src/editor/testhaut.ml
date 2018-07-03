730b26575f39ba56e5410660186ba6362021cdf6
Alexandre Perge <alexandre.perge@univ-tlse3.fr>
2018-07-03 14:15:04 +0200

    suppression buffer output suite

--- src/editor/testhaut.ml
+++ src/editor/testhaut.ml
@@ -130,20 +130,14 @@ let editor_input_suite = Ocaml_mode.create_ocaml_editor (Tyxml_js.To_dom.of_div 
 let ace_input_suite = Ocaml_mode.get_editor editor_input_suite 
 let _ = Ace.set_contents ace_input_suite ("[]");
         Ace.set_font_size ace_input_suite 18;;
-
-let output_suite_editor = find_component "learnocaml-tab-suite-output" 
-let editor_output_suite = Ocaml_mode.create_ocaml_editor (Tyxml_js.To_dom.of_div output_suite_editor) 
-let ace_output_suite = Ocaml_mode.get_editor editor_output_suite 
-let _ = Ace.set_contents ace_output_suite ("[]");
-        Ace.set_font_size ace_output_suite 18;;
-       
+      
 
 let save_suite () =
   let name = Js.to_string name##.value in
   let ty = Js.to_string ty##.value in
   let type_question = Suite in
   let input = Ace.get_contents ace_input_suite in
-  let output = Ace.get_contents ace_output_suite in
+  let output = "" in
   let extra_alea = 0 in
   let question = {name; ty; type_question; input; output; extra_alea} in
   let testhaut =  get_testhaut id in
@@ -153,11 +147,7 @@ let save_suite () =
   in
   let testhaut = StringMap.add question_id question testhaut in
   save_testhaut testhaut id
-
 ;;
-  
-
-
 
 let save_solution () =
   let name = Js.to_string name##.value in
@@ -206,7 +196,6 @@ let _ = match arg "questionid" with
              | Suite ->
                 begin
                   Ace.set_contents ace_input_suite input;
-                  Ace.set_contents ace_output_suite output;
                   name_elt##.value:=Js.string name;
                   suite##.checked := Js.bool true;
                   ty_elt##.value:=Js.string ty;

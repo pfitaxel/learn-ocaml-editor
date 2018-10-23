597d869a5f1c3f9dfdc181f4edd107416dae7da4
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2018-10-23 19:49:23 +0200

    Check-Test.ml(editor_lib.typecheck_spec): Translate error line numbers
    and replace "Dummy_Params" with a shorter identifier, "Mock"

--- src/editor/editor_lib.ml
+++ src/editor/editor_lib.ml
@@ -228,19 +228,27 @@ let checkbox_creator string cas id =
   Tyxml_js.Of_dom.of_input dom_chk
 
 let test_lib_prepare =
-{|module Dummy_Functor (Introspection : Introspection_intf.INTROSPECTION) =
+{|module Wrapper (Introspection : Introspection_intf.INTROSPECTION) =
   struct
-  module Dummy_Params = struct
+  module Mock = struct
     let results = ref None
     let set_progress _ = ()
     let timeout = None
     module Introspection = Introspection
     end
-  module Test_lib = Test_lib.Make(Dummy_Params)
+  module Test_lib = Test_lib.Make(Mock)
   module Report = Learnocaml_report;;
-  open Dummy_Params
+  open Mock
   let code_ast = (failwith "WIP" : Parsetree.structure);;
 |}
+
+let offset_test_lib_prepare =
+  let rec num_occs s i c num_acc =
+    if i > String.length s then num_acc
+    else match String.index_from_opt s (i + 1) c with
+         | None -> num_acc
+         | Some i -> num_occs s i c (num_acc + 1)
+  in num_occs test_lib_prepare (-1) '\n' 0
 
 let with_test_lib_prepare string = test_lib_prepare ^ string ^ " end";;
 
@@ -251,8 +259,10 @@ let typecheck_spec_aux set_class ace_t editor_t top string=
       match res with
       | Toploop_results.Ok ((), warnings) -> None, warnings
       | Toploop_results.Error (err, warnings) -> Some err, warnings in
+    let shift_loc (l, c) = (l - offset_test_lib_prepare, c) in
     let transl_loc { Toploop_results.loc_start ; loc_end } =
-      { Ocaml_mode.loc_start ; loc_end } in
+      { Ocaml_mode.loc_start = shift_loc loc_start ;
+        Ocaml_mode.loc_end = shift_loc loc_end } in
     let error = match error with
       | None -> None
       | Some { Toploop_results.locs ; msg ; if_highlight } ->
